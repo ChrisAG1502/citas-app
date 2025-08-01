be6c34569e3a6638eee2d309e0d9956da8a31c14
Chakistrikis <christian.aguilerayamal@gmail.com>
2025-07-31 22:27:10 -0600

    Feat: Functional user-dashboard (Just the beginning)

--- src/main/java/azc/uam/app/controller/DashboardController.java
+++ src/main/java/azc/uam/app/controller/DashboardController.java
@@ -1,5 +1,9 @@
 package azc.uam.app.controller;
 
+import azc.uam.app.dto.UsuarioDTO;
+import azc.uam.app.model.entity.Usuario;
+import azc.uam.app.model.enums.Genero;
+import azc.uam.app.service.persistance.UsuarioService;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,9 +14,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/dashboard")
 public class DashboardController {
 
+    private final UsuarioService usuarioService;
+
+    public DashboardController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
     @GetMapping("/cliente")
     public String dashboardCliente(Authentication authentication, Model model) {
+        String correo = authentication.getName();
+        Usuario usuario = usuarioService.findByCorreo(correo);
         model.addAttribute("usuario", authentication.getName());
+        model.addAttribute("customerName", usuario.getNombre());
+        if(usuario.getGenero().equals(Genero.MASCULINO)) {
+            model.addAttribute("gender", "Bienvenido ");
+        }
+        else{
+            model.addAttribute("gender", "Bienvenida ");
+        }
         return "dashboard-cliente";
     }
 
